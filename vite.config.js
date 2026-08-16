cd267e72a67c17eb659482bf524516b8a7c3bf8e
unknown <jhanicakrizelle14@gmail.com>
2026-08-16 14:56:11 +0800

    Add Github Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [vue()],
+  base: '/chan-module7-vue-system/',
 })
